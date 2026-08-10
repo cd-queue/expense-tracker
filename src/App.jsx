60e2c25a7c909c22ec06946578cffd41f6d11160
Aja <au4588219@gmail.com>
2026-08-10 21:06:27 +0530

    Added Project Structure.

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [expenses, setExpenses] = useState([]);
+  return (
+    <>
+      <h1>Expense Tracker</h1>
+    </>
+  )
+}
+
+export default App
